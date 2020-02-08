50ce316e3a29ad12bb4e21b101e443f11175db78
Rolando Javier <rolando.ramos2@unmsm.edu.pe>
2020-02-08 08:53:52 -0500

    inicio proyecto

---
+++ src/Configuracion/Conf.js
@@ -0,0 +1,6 @@
+
+   // const CONFIG1= 'https://sigapdev-conrecibospre-back.herokuapp.com'   
+   //const CONFIG= 'https://sigapdev2-consultarecibos-back.herokuapp.com/'   
+   const CONFIG1= 'http://localhost:8080/';
+
+export default CONFIG1;
