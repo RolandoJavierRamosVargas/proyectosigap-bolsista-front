3a4d2fec8b909e855e920431a506f5ccc1e4ba35
Rolando Javier <rolando.ramos2@unmsm.edu.pe>
2020-02-11 16:03:28 -0500

    version con todo implementado

--- src/Configuracion/Config.js
+++ src/Configuracion/Config.js
@@ -1,4 +1,5 @@
 
    // const CONFIG= 'https://sigapdev2-consultarecibos-back.herokuapp.com/'   
-   const CONFIG= 'http://localhost:8080/';
+   // const CONFIG= 'http://localhost:8080/';
+   const CONFIG='https://proyectosigapfisi.herokuapp.com/';
 export default CONFIG;
